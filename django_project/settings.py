3a93f60697447070f504c1cfdffb1153b3afe2a5
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2022-12-14 09:21:30 +0000

    docs: update README.md

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@q43!m00r*qoc%rfwsmpmqatk&pc%&*!=8k5f$z=2p$x*hicp1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
